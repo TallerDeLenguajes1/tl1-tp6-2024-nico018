486d139817f7d69bb8b9541051fa0e29ab71191a
nico018 <nicolas12velasco@gmail.com>
2024-05-27 19:10:10 -0300

    se agrego ejercicio1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InvertirNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número:");
+            string entrada = Console.ReadLine();
+
+            if (int.TryParse(entrada, out int numero))
+            {
+                if (numero > 0)
+                {
+                    int numeroInvertido = InvertirNumero(numero);
+                    Console.WriteLine($"El número invertido es: {numeroInvertido}");
+                }
+                else
+                {
+                    Console.WriteLine("El número debe ser mayor a 0.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El texto ingresado no es un número válido.");
+            }
+        }
+
+        static int InvertirNumero(int numero)
+        {
+            int invertido = 0;
+            while (numero > 0)
+            {
+                int digito = numero % 10;
+                invertido = invertido * 10 + digito;
+                numero /= 10;
+            }
+            return invertido;
+        }
+    }
+}
